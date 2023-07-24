3fb05c3acb72d16adae6728720093c1a06d9fa72
Raffael47 <raffaelwdj@gmail.com>
2023-07-24 09:38:59 +0700

    comman-antum

--- database/blog_keywords.sql
+++
@@ -1,9 +0,0 @@
-USE mini_project;
-
-CREATE TABLE mini_project.blog_keywords (
-	blogId INT NOT NULL,
-    keywordsId INT NOT NULL,
-    PRIMARY KEY (blogId, keywordsId));
-    
-SELECT * FROM blog_keywords;
-    
